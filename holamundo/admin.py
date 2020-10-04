086d41e74dbdc20a8dd44dbac3444e154bf5ccb1
castillo098 <losvideojuegos7268098@gmail.com>
2020-10-04 12:19:38 -0500

    Se creo otro modelo y se modifico el tipo de dato

--- holamundo/admin.py
+++ holamundo/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Carrera
 from .models import Paralelo
+from .models import Estudiante
 
 # Register your models here.
 admin .site.register(Carrera)
 admin .site.register(Paralelo)
+admin .site.register(Estudiante)
 
--- holamundo/models.py
+++ holamundo/models.py
@@ -2,14 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Carrera(models.Model):
-    nombreCarrera=models.CharField(max_length=200,default="N/A")
-    numeroCiclo=models.IntegerField(default=5)
-    numeroCreditos=models.IntegerField(default=120)
-    codigoCarrera=models.CharField(max_length=10,default="N/A")
-    numeroDocentes=models.IntegerField(default=5)
+    nombreCarrera=models.CharField(max_length=200)
+    numeroCiclo=models.IntegerField(max_length=5)
+    numeroCreditos=models.IntegerField(max_length=120)
+    codigoCarrera=models.CharField(max_length=10)
+    numeroDocentes=models.IntegerField(max_length=5)
 
 class Paralelo(models.Model):
-    carrera=models.ForeignKey(Carrera,on_delete=models.CASCADE,default=None)
-    ciclo=models.IntegerField(default=5)
-    nombre=models.CharField(max_length=10,default="N/A")
-    numeroEstudiantes=models.IntegerField(default=5)
+    carrera=models.ForeignKey(Carrera,on_delete=models.CASCADE)
+    ciclo=models.IntegerField(max_length=5)
+    nombre=models.CharField(max_length=10)
+    numeroEstudiantes=models.IntegerField(max_length=5)
+
+class Estudiante(models.Model):
+    Cedula=models.CharField(max_length=10)
+    Nombre=models.CharField(max_length=200)
+    Apellido=models.CharField(max_length=200)
+    Edad=models.IntegerField(max_length=2)
+    # Carrera=models.ForeignKey(Carrera,on_delete=models.CASCADE)
+    # Paralelo=models.ForeignKey(Paralelo,on_delete=models.CASCADE)
+    Telefono=models.CharField(max_length=10)
+    email=models.CharField(max_length=200)
+    direccion=models.CharField(max_length=200)
+    Ocupaciones=models.CharField(max_length=200)
